fcb47e35b56cf7b68c77abe3bbae9240fa5e18ac
Peter Robinson <peter.robinson@softwire.com>
2020-09-08 16:22:21 +0100

    Add simple FizzBuzz script

--- app/src/main/java/com/example/fizzbuzzkotlin/fizzbuzz.kt
+++ app/src/main/java/com/example/fizzbuzzkotlin/fizzbuzz.kt
@@ -1,5 +1,41 @@
 package com.example.fizzbuzzkotlin
 
 fun main() {
-    println("Hello World!")
+    fizzbuzz(200)
+}
+
+private fun fizzbuzz(x: Int) {
+    val allResults = mutableListOf<String>()
+    for (i in 1..x) {
+        val result = mutableListOf<String>()
+        if (i % 3 == 0) result.add("Fizz")
+        if (i % 5 == 0) result.add("Buzz")
+        if (i % 7 == 0) result.add("Bang")
+        if (i % 11 == 0) {
+            result.clear()
+            result.add("Bong")
+        }
+        if (i % 13 == 0) {
+            for ((index, string) in result.withIndex()) {
+                if (hasLeadingB(string)) {
+                    result.add(index, "Fezz")
+                    break
+                }
+            }
+        }
+        if (result.isNullOrEmpty()){
+            allResults.add(i.toString())
+        } else {
+            allResults.add(result.joinToString(""))
+        }
+    }
+    val output = allResults.joinToString("\n")
+    println(output)
+}
+
+private fun hasLeadingB(arg: String): Boolean {
+    if (arg[0] == 'B') {
+        return true
+    }
+    return false
 }
