1967e8c2a38c91ca37d9eb50753bef5d5fb740c0
Peter Robinson <peter.robinson@softwire.com>
2020-09-09 12:21:22 +0100

    Add custom user rules

--- app/src/main/java/com/example/fizzbuzzkotlin/fizzbuzz.kt
+++ app/src/main/java/com/example/fizzbuzzkotlin/fizzbuzz.kt
@@ -3,38 +3,60 @@ package com.example.fizzbuzzkotlin
 import java.lang.Exception
 import java.util.Scanner
 
+data class Rule(
+    val word: String,
+    val function: (currentOutput: MutableList<String>, ruleWord: String) -> MutableList<String>
+)
 
 fun main() {
+    val reader = Scanner(System.`in`)
+    val ruleDispatcher = mapOf<String, (MutableList<String>, String) -> MutableList<String>>(
+        "append" to ::appendRule,
+        "clear-and-replace" to ::clearAndReplaceRule,
+        "add-before-first-B" to ::addBeforeFirstBRule,
+        "reverse" to ::reverseRule,
+        "flip-strings" to ::flipAllStringsRule
+    )
+    println("Welcome to FizzBuzz")
+    println("Current rules available are:")
+    for (rule in ruleDispatcher.keys) {
+        println(rule)
+    }
     try {
-        val reader = Scanner(System.`in`)
+        print("Enter new rule to add: ")
+        val newRule: String = readLine()!!
+        print("Enter rule keyword: ")
+        val newRuleKeyword: String = readLine()!!
+        print("Enter new rule number: ")
+        val newRuleNumber: Int = reader.nextInt()
+        val userRules = mapOf<Int, Rule>(
+            newRuleNumber to Rule(newRuleKeyword, ruleDispatcher[newRule] ?: error("Invalid choice of new rule"))
+        )
         print("Enter limit: ")
         val upperLimit:Int = reader.nextInt()
-        fizzbuzz(upperLimit)
+        fizzbuzz(upperLimit, userRules)
     } catch (e: Exception) {
-        println("Invalid input")
+        println(e)
     }
 }
 
-private fun fizzbuzz(upperLimit: Int = 100) {
-    data class Rule(
-        val word: String,
-        val function: (currentOutput: MutableList<String>, ruleWord: String) -> MutableList<String>
-    )
-    val rules = mapOf<Int, Rule>(
+private fun fizzbuzz(upperLimit: Int = 100, userRules: Map<Int, Rule>) {
+    val rules = mutableMapOf<Int, Rule>(
         3 to Rule("Fizz", ::appendRule),
         5 to Rule("Buzz", ::appendRule),
         7 to Rule("Bang", ::appendRule),
         11 to Rule("Bong", ::clearAndReplaceRule),
         13 to Rule("Fezz", ::addBeforeFirstBRule),
         17 to Rule("", ::reverseRule),
-        19 to Rule("", ::flipAllStrings)
+        19 to Rule("", ::flipAllStringsRule)
     )
+    rules.putAll(userRules)
 
     val allResults = mutableListOf<String>()
     for (i in 1..upperLimit) {
         var result = mutableListOf<String>()
         for ((ruleNumber, ruleDefinition) in rules.entries) {
-            result = generalRuleCase(i, result, ruleNumber, ruleDefinition.word, ruleDefinition.function)
+            result = generalRuleCase(i, result, ruleNumber, ruleDefinition)
         }
         allResults.add(generateOutputLine(result, i))
     }
@@ -46,12 +68,11 @@ private fun generalRuleCase(
     countNumber: Int,
     currentOutput: MutableList<String>,
     ruleNumber: Int,
-    ruleWord: String,
-    ruleFunction: (currentOutput: MutableList<String>, ruleWord: String) -> MutableList<String>
+    rule: Rule
 ): MutableList<String> {
     var result = currentOutput
     if (countNumber % ruleNumber == 0) {
-        result = ruleFunction(currentOutput, ruleWord)
+        result = rule.function(currentOutput, rule.word)
     }
     return result
 }
@@ -82,7 +103,7 @@ private fun addBeforeFirstBRule(currentOutput: MutableList<String>, ruleWord: St
     return currentOutput
 }
 
-private fun flipAllStrings(currentOutput: MutableList<String>, ruleWord: String): MutableList<String> {
+private fun flipAllStringsRule(currentOutput: MutableList<String>, ruleWord: String): MutableList<String> {
     val newOutput = mutableListOf<String>()
     for (string in currentOutput) {
         newOutput.add(string.reversed())
